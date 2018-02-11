b610921c8a7de5215f34a1835643cdc25969d8a8
Mohammed Boukedjar <mboukedjar@gmail.com>
2018-02-11 12:12:58 +0100

    Hello

---
+++ batch-collecte/src/main/java/mob/collecte/BatchCollecteApplication.java
@@ -0,0 +1,14 @@
+package mob.collecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCollecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCollecteApplication.class, args);
+		
+		// some shit...
+	}
+}
